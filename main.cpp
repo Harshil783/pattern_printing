d208c2898ca5302191dacb810c12c800c0762841
Harshil783 <harshilpandey57@gmail.com>
2021-10-17 21:29:29 +0530

    Big Updates

--- main.cpp
+++ main.cpp
@@ -6,27 +6,27 @@ int main(int argc, char const *argv[])
     std::cout << "\t\t\t\t\t\tAll The Patterns";
     int n = 5;
     std::cout << "\n";
-    std::cout<<"Rectangle\n";
+    std::cout << "Rectangle\n";
     pattern1(n);
     std::cout << "\n";
 
-    std::cout<<"Right Pyramid\n";
+    std::cout << "Right Pyramid\n";
     pattern2(n);
     std::cout << "\n";
-    
-    std::cout<<"Inverted half pyramid\n";
+
+    std::cout << "Inverted half pyramid\n";
     pattern3(n);
     std::cout << "\n";
 
-std::cout<<"Chritmas Tree\n";
+    std::cout << "Chritmas Tree\n";
     pattern4(n);
     std::cout << "\n";
 
-std::cout<<"Half Pyramid after 180 deg rotation\n";
+    std::cout << "Half Pyramid after 180 deg rotation\n";
     pattern4_2(n);
     std::cout << "\n";
 
-std::cout<<"\t\t\t\t\t\tNumber Patterns\n";
+    std::cout << "\t\t\t\t\t\tNumber Patterns\n";
     pattern5(n);
     std::cout << "\n";
 
@@ -39,15 +39,15 @@ std::cout<<"\t\t\t\t\t\tNumber Patterns\n";
     pattern9(n);
     std::cout << "\n";
 
-    std::cout<<"rhombus 180 deg rotation\n";
+    std::cout << "rhombus 180 deg rotation\n";
     pattern10(n);
     std::cout << "\n";
 
-std::cout<<"rhombus\n";
+    std::cout << "rhombus\n";
     pattern11(n);
     std::cout << "\n";
 
-std::cout<<"Number Traingles\n";
+    std::cout << "Number Traingles\n";
     pattern12(n);
     std::cout << "\n";
 
@@ -56,8 +56,8 @@ std::cout<<"Number Traingles\n";
 
     pattern14(n);
     std::cout << "\n";
-    
-    std::cout<<"Butterfly Pattern Solid\n";
+
+    std::cout << "Butterfly Pattern Solid\n";
     pattern7(n);
     std::cout << "\n";
 
